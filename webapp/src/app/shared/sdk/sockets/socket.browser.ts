f92a7f72727bc58a0b0e84a81fe24c99f17bbe44
Antares Farias <afarias@itexico.com>
2018-01-25 16:29:23 -0600

    Fix for new angular-cli and fireloop beta_2.7

--- webapp/src/app/shared/sdk/sockets/socket.browser.ts
+++ webapp/src/app/shared/sdk/sockets/socket.browser.ts
@@ -2,6 +2,15 @@
 import * as io from 'socket.io-client';
 export class SocketBrowser {
   connect(url: any, options: any) {
+    console.log(options);
+    options.transports = [
+      'websocket', 
+      'flashsocket', 
+      'htmlfile', 
+      'xhr-polling', 
+      'jsonp-polling', 
+      'polling'
+    ]
     return io(url, options);
   }
 }
